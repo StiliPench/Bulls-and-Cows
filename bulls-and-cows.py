4d492f3e6870b6aa34686592c149417a3c48209d
Stilyan <stilipench@gmail.com>
2022-06-05 19:29:42 +0300

    Add a function which returns the user input if it is credible

--- bulls-and-cows.py
+++ bulls-and-cows.py
@@ -21,3 +21,15 @@ def computerNum():
         number = random.randint(1234, 9876)
         if diffDigits(number) and not(hasZero(number)):
             return number
+
+def userInput():
+    while True:
+        try:
+            number = int(input("Enter a number: "))
+            if(hasZero(number) or not(diffDigits(number)) or number<1234 or number>9876):
+                print("Incorrect input. Please enter a number which has 4 different digits excluding 0!")
+                continue
+            break
+        except ValueError:
+            print("Incorrect input. Please enter a number which has 4 different digits excluding 0!")
+    return number
